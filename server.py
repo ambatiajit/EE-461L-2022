c9098d615b8ec2e621b7add9655b73f061e14faa
ambatiajit <61094256+ambatiajit@users.noreply.github.com>
2022-02-16 16:15:11 -0600

    Update server.py

--- server.py
+++ server.py
@@ -15,6 +15,29 @@ class LoginForm(FlaskForm):
     submit = SubmitField(label='Log in')
 
 
+# def customEncrypt(plain_text, num_positions, direction):
+#     encrypted_text = list(plain_text)[::-1]
+#
+#     for i in range(len(encrypted_text)):
+#         curr_ascii_val = ord(encrypted_text[i])  # store ascii value
+#
+#         if direction == 1:  # add
+#             new_ascii_val = (curr_ascii_val + num_positions) % 127
+#             if new_ascii_val < curr_ascii_val:  # take care of values less than 34
+#                 new_ascii_val += 34
+#         else:  # subtract
+#             new_ascii_val = curr_ascii_val - num_positions
+#             if new_ascii_val < 34:  # take care of values less than 34
+#                 new_ascii_val += 93
+#
+#         new_letter = chr(new_ascii_val)
+#         encrypted_text[i] = new_letter
+#
+#     result = ''.join(encrypted_text)  # convert result (list) into string
+#
+#     return result
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -22,7 +45,13 @@ def home():
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    if request.method == "POST":
+        userid = form.userid.data       # stores userID entered by user
+        password = form.password.data   # stores password entered by user
+        # need to encrypt password using customEncrypt
+        print(userid, password)
+    return render_template('login.html', form=form)
 
 
 @app.route('/register', methods=["GET", "POST"])
@@ -30,8 +59,19 @@ def register():
     return render_template('register.html')
 
 
+@app.route('/resources')
+def resources():
+    return render_template('resources.html')
 
 
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+
+@app.route('/data_access')
+def data_access():
+    return render_template('data_access.html')
 
 
 if __name__ == '__main__':
